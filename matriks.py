1b418facc9d183ab9909dcda5efe9dd118da73a3
VansonK <vansonkurnialim@gmail.com>
2023-04-16 15:09:50 +0700

    tes

--- matriks.py
+++ matriks.py
@@ -1,33 +1,35 @@
 from hitungbaris import *
+from length import *
+from Bacamatriks import *
+from summonjin import summonjin
+
 def matriks() :
     f = open('user.csv')
     baris = hitungbaris('user.csv') + 1
     global user
-    user = [0 for i in range(baris)]
+    user = [0 for i in range(baris+1)]
     for i in range(baris) :
         cc = f.readline()
         user[i] = cc
+    user[baris] = ''
     f.close()
 
     f = open('candi.csv')
     baris = hitungbaris('candi.csv') + 1
     global candi
-    candi = [0 for i in range(baris)]
+    candi = [0 for i in range(baris+1)]
     for i in range(baris) :
         cc = f.readline()
         candi[i] = cc
+    candi[baris] = ''
     f.close()
 
     f = open('bahan_bangunan.csv')
     baris = hitungbaris('bahan_bangunan.csv') + 1
     global bahan
-    bahan = [0 for i in range(baris)]
+    bahan = [0 for i in range(baris+1)]
     for i in range(baris) :
         cc = f.readline()
         bahan[i] = cc
+    bahan[baris] = ''
     f.close()
-  
-matriks()
-print(user)
-print(candi)
-print(bahan)
