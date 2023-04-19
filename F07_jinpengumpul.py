6bb2cddd03a0e24c66fbd258aa4573e0b74d3f26
Osamah18 <130562655+Osamah18@users.noreply.github.com>
2023-04-19 23:48:00 +0700

    Create F07_jinpengumpul.py

---
+++ F07_jinpengumpul.py
@@ -0,0 +1,9 @@
+import random
+
+def kumpul():
+    pasir = random.randint(0,5)
+    batu = random.randint(0,5)
+    air = random.randint(0,5)
+
+    print(f"Jin menemukan {pasir} pasir, {batu} batu, dan {air} air.")
+        
