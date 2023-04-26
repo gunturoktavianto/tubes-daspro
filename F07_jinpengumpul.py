b3e487a53b7bb459b216a8a46fee1940b92bef52
Osamah Azhar <osamahazhar18@gmail.com>
2023-04-26 14:52:22 +0700

    Memperbaiki fungsi biar bahan ditambahkan ke matriks

--- F07_jinpengumpul.py
+++ F07_jinpengumpul.py
@@ -5,7 +5,8 @@ def kumpul():
     batu = random.randint(0,5)
     air = random.randint(0,5)
     
-    
-
+    bahan[1][2] += pasir 
+    bahan[2][2] += batu
+    bahan[3][2] += air
 
     print(f"Jin menemukan {pasir} pasir, {batu} batu, dan {air} air.")
