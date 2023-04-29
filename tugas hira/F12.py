3256974f063898a83b6e8e0b56d18fb17d59f22e
hiirr <124429888+hiirr@users.noreply.github.com>
2023-04-29 20:39:41 +0700

    Update F12.py
    ayamberkokok diedit

--- tugas hira/F12.py
+++ tugas hira/F12.py
@@ -1,24 +1,28 @@
 from matriks import *
-from F11 import *
+import time
 
-def ayamberkokok(jumlah): # jumlah yang dimasukkan harus {length(candi) - 1} karena dikurang header
+def ayamberkokok(): 
+    jumlah = length(candi) - 1 # jumlah harus {length(candi) - 1} karena dikurang header
     if jumlah < 100 :
         print("Kukuruyuk.. Kukuruyuk..")
         print("")
+        time.sleep(1)
         print("Jumlah Candi: " + str(jumlah))
         print("")
+        time.sleep(1)
         print("Selamat, Roro Jonggrang memenangkan permainan!")
         print("")
+        time.sleep(1)
         print("*Bandung Bondowoso angry noise*")
+        time.sleep(1)
         print("Roro Jonggrang dikutuk menjadi candi.")
     else:
         print("Kukuruyuk.. Kukuruyuk..")
         print("")
+        time.sleep(1)
         print("Jumlah Candi: " + str(jumlah))
         print("")
+        time.sleep(1)
         print("Yah, Bandung Bondowoso memenangkan permainan!")
-
-
-candi = hancurkancandi(candi)
-
-ayamberkokok(length(candi)-1)
+        
+ayamberkokok()
